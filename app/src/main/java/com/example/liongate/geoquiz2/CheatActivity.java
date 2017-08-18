9d06f96c35e0d12f6c093e78db92b0824c9442f5
KuvikaM <kmgroup67@gmail.com>
2017-08-19 01:15:11 +0200

    добавили передачу инф с интентом

--- app/src/main/java/com/example/liongate/geoquiz2/CheatActivity.java
+++ app/src/main/java/com/example/liongate/geoquiz2/CheatActivity.java
@@ -1,13 +1,30 @@
 package com.example.liongate.geoquiz2;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
 public class CheatActivity extends AppCompatActivity {
 
+
+    public static final String EXTRA_ANSWER_IS_TRUE = "com.geo.answer_is_true";
+
+    private boolean mAnswerIsTrue;
+
+    public static Intent newIntent(Context packageContext,boolean answerIsTrue){
+
+        Intent intent = new Intent(packageContext,CheatActivity.class);
+        intent.putExtra(EXTRA_ANSWER_IS_TRUE,answerIsTrue);
+
+        return intent;
+
+    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cheat);
+
+        mAnswerIsTrue = getIntent().getBooleanExtra(EXTRA_ANSWER_IS_TRUE,false);
     }
 }
